f7d1f459de6f17a96373f6bc4d4099a8d60797fe
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-10-02 07:27:39 +0700

    Update perpus_1.sql

--- perpus_1.sql
+++ perpus_1.sql
@@ -15,6 +15,16 @@ CREATE TABLE `reports` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `transaction_date` date NOT NULL,
+  `transaction_type` varchar(50) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -24,7 +34,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
   
 ALTER TABLE `books`
   ADD CONSTRAINT `fk_books_users` FOREIGN KEY (`pemilik`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
